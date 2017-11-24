8f20d9b3bc3ad500b57387f2e2b45aec4d6ec762
Cristian Cortés <cristian.cortes@brm.com.co>
2017-11-23 19:22:43 -0500

    corte final

---
+++ src/es6/pts-co.init.js
@@ -0,0 +1,145 @@
+jQuery(document).ready(()=>{
+
+	const slideHome = {
+	    slidesToShow: 1,
+	    arrows: true,
+	    mobileFirst: true,
+	    nextArrow:'<button type="button" class="icon icon-next slick-next"> ',
+	    prevArrow:'<button type="button" class="icon icon-prev slick-prev"> '
+  	};
+
+  	jQuery('.slider').slick(slideHome);
+
+
+  	/*btns*/
+
+  	jQuery('.btn').click( function (e) {
+  		let type = jQuery(this).attr('class').split(/\s+/);
+  		
+  		let btnType = type.length - 1
+
+  		switch(type[btnType]){
+
+  			case('btn-menu'):
+
+  				jQuery('.menu').toggleClass('active');
+  				setTimeout(()=>{
+  					jQuery('body').toggleClass('body-fixed');
+  				}, 1000)
+
+  				break;
+
+  			case('btn-who'):
+
+  				e.preventDefault();
+		  		jQuery('#who-modal').modal('show');
+  				break;
+
+
+  			case('btn-beneficios'):
+
+  				e.preventDefault();
+
+		  		jQuery('#beneficios-modal').modal('show');
+
+  				break;
+
+  			case('btn-close'):
+
+  				if( jQuery(this).parent().is('.container-menu') ){
+            e.preventDefault();
+	  			  
+	  				jQuery('.menu').toggleClass('active');
+  					
+  					jQuery('body').toggleClass('body-fixed');
+
+  				}else{
+            jQuery(this).parent().removeClass('active')
+            jQuery('.btn-vacante').removeClass('active')
+          }
+  				break;
+
+  		};
+
+
+  	} );
+
+    //Desplegable de vacantes//
+    jQuery('.btn-vacante').click(function(e) {
+      e.preventDefault();
+      jQuery('.btn-vacante').removeClass('active');
+
+      jQuery(this).toggleClass('active');
+      jQuery('.oferta-desc').removeClass('active');
+
+      jQuery(this).parent().parent().find('.oferta-desc').toggleClass('active');
+
+     
+    });
+
+
+    /*Scroll suavizado con anclas*/
+    const go = (val) =>{
+      
+      let pos = jQuery(val).attr('href');
+
+      jQuery('html, body').animate({
+        scrollTop: jQuery(pos).offset().top
+      }, 1000);
+
+    };
+
+    jQuery('.navigator a').click( function (e) {
+      e.preventDefault();
+      go(this);
+    });
+
+    /*scroll suavizado con menu*/
+
+    jQuery('.menu li a').click(function(e) {
+     
+     let that = this;
+     let href = window.location.pathname;
+     let goTo = jQuery(this).prop('pathname')
+
+     console.log(goTo)
+
+     // console.log(href)
+
+      if(jQuery(this).prop('hash') && goTo == href ){
+         e.preventDefault();
+        go(this);
+        jQuery('.menu').toggleClass('active');
+
+        jQuery('body').removeClass('body-fixed');
+
+      }
+    });
+
+
+    //Mobile sticky headers//
+
+    if( jQuery(window).width() < 768 ){
+      jQuery('header').affix({
+        offset: {
+          top: 25
+        }
+      });
+
+    };
+
+    const locacion = window.location.hash
+
+    if (locacion != ''){
+
+      jQuery('html, body').animate({
+        scrollTop: jQuery(locacion).offset().top
+      }, 1000);
+
+
+
+    };
+
+
+/*No pasar de aquí*/
+});
---
+++ src/es6/pts-co.validate.js
@@ -0,0 +1,95 @@
+const $textarea    = jQuery('.form-group textarea');
+const $input     = jQuery('.form-group input');
+const $form      = jQuery('#aliados');
+const $btnSubmit = jQuery('#aliados .btn-submit');
+const $cal       = jQuery('#aliados');
+
+//Estado activo de los campos//
+const campoActive = (el) =>{
+
+  jQuery(el)
+    .focusin(function() {
+      jQuery(this).parent().addClass('active')
+    
+    })
+    .focusout(function() {
+
+      let $self = jQuery(this);
+
+      if (jQuery($self).val() != '' ){
+        jQuery($self).parent().addClass('active')
+
+      }else{
+        jQuery($self).parent().removeClass('active')
+
+      }
+    });
+
+};
+
+
+jQuery(document).ready( ()=>{
+
+  //inputs activos
+  campoActive($input);
+  campoActive($textarea);
+
+
+  //metodo para aceptar texto con espacios y caracteres especiales
+
+  jQuery.validator.addMethod('letras', function(val, el){
+    return this.optional(el) || /^[a-z" "ñÑáéíóúÁÉÍÓÚ,.;]+$/i.test(val);
+  });
+
+
+  //Ubicacion de mensaje de error//
+  const errorPlacement = (error, element)=>{
+    error.insertAfter(element.parent())
+  };
+
+  $form.validate({
+    debug: true,
+
+    errorElement: 'div',
+    errorClass: 'msn-place',
+
+    rules: {
+
+      nombre:       {required:true,letras:true},
+      celular:     {required:true, digits:true, minlength:9, maxlength:10 },
+      email:      {required:true, email:true},
+      comentario:    { required:true, maxlength:250  }
+
+    },
+
+
+    messages: {
+      nombre:{
+        required:'Indicanos tu nombre',
+        letras: 'Solo ingresa letras'
+      },
+      celular:{
+        required: 'Indicanos un tel&eacute;fono de contacto' ,
+        digits:'Solo ingresa n&uacute;meros',
+        minlength: 'N&uacute;mero no v&aacute;lido',
+        maxlength: 'N&uacute;mero no v&aacute;lido'
+      },
+      email:{
+        required: 'Indicanos un email' ,
+        email: 'Formato de email inv&aacute;lido'
+      },
+      comentario:{
+        required: 'C&uacute;entanos algo',
+        maxlength: 'El mensaje es muy largo'
+
+      }
+
+    },
+
+    errorPlacement
+
+  });
+
+  
+/*No pasar de acá para el DOM ready*/
+});
